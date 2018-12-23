507cb78cf68781e4bcb8b2cc93e1ae48307978c6
David Avigad <david@lusha.co>
2018-12-24 01:26:25 +0200

    adding comments

--- node_tar5.2/index.js
+++ node_tar5.2/index.js
@@ -1,21 +1,27 @@
+// NPM Modules
 const express = require('express')
 const morgan = require('morgan')
 
+// Internal Modules
 const ctrl = require('./controller')
 const asyncWrapper = require('./async.wrapper')
 
+// Establish app()
 const app = express()
 const port =  process.env.PORT || 3000
 
+// Middleware(s)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
+// Routes
 app.get('/posts',    asyncWrapper(ctrl.getAllPosts))
 app.get('/post/:id', asyncWrapper(ctrl.getPost))
 app.put('/post/:id', asyncWrapper(ctrl.editPost))
 app.post('/post',    asyncWrapper(ctrl.addPost))
 app.post('/remove',  asyncWrapper(ctrl.removePost))
 
+// Run the server
 app.listen(port, 
     () => console.log('Express server ready for requests on:', port))
